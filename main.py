7ba4808a2955cbeea161c858fc8d538407137e5c
Przemysław Łada <przlada@gmail.com>
2018-04-29 12:31:03 +0200

    fix

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import time
 import datetime
 import logging
 
+class OneLineExceptionFormatter(logging.Formatter):
+    # def formatException(self, exc_info):
+    #     result = super().formatException(exc_info)
+    #     return repr(result)
+ 
+    def format(self, record):
+        result = super().format(record)
+        if record.exc_text:
+            result = result.replace("\n", " | ")
+        return result
+
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
@@ -11,7 +23,9 @@ fh.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = OneLineExceptionFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 
