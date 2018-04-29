e21343cd450d8d86a1da3f2d6d82b6836426733c
Przemysław Łada <przlada@gmail.com>
2018-04-29 14:00:19 +0200

    logzio handler

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 import time
 import datetime
+
+# class OneLineExceptionFormatter(logging.Formatter):
+#     def format(self, record):
+#         result = super().format(record)
+#         if record.exc_text:
+#             result = result.replace("\n", " | ")
+#         return result
+
 import logging
+import logging.config
 
-class OneLineExceptionFormatter(logging.Formatter):
-    # def formatException(self, exc_info):
-    #     result = super().formatException(exc_info)
-    #     return repr(result)
- 
-    def format(self, record):
-        result = super().format(record)
-        if record.exc_text:
-            result = result.replace("\n", " | ")
-        return result
-
-
+# Say i have saved my configuration under ./myconf.conf
+logging.config.fileConfig('logging_conf.conf')
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
-fh = logging.FileHandler('logger.log')
+
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('logger_new.log')
 fh.setLevel(logging.DEBUG)
 
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+# ch = logging.StreamHandler()
+# ch.setLevel(logging.DEBUG)
 
 # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-formatter = OneLineExceptionFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# formatter = OneLineExceptionFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
+# fh.setFormatter(formatter)
+# ch.setFormatter(formatter)
 
 logger.addHandler(fh)
-logger.addHandler(ch)
+# logger.addHandler(ch)
 
 def error_function():
     raise Exception('Error info')
@@ -38,11 +40,13 @@ def error_function():
 while True:
     logger.info('Logger info msg '+str(datetime.datetime.now()))
     logger.debug('Logger info debug '+str(datetime.datetime.now()))
+    logger.info('Some extra value msg before', extra={'extra_key':'before'})
     try:
         error_function()
     except:
         logger.exception("Some exception occure")
     time.sleep(10)
+    logger.info('Some extra value msg after', extra={'extra_key':'after'})
 
 
 
